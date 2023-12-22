d0802e6a048d7928107acdafe37fedffe5430bac
IDewaAgungRama <116362990+IDewaAgungRama@users.noreply.github.com>
2023-12-22 15:32:53 +0700

    Add files via upload

---
+++ TUBESMANPROSI/homepage.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="Homepage" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+
+<link rel="stylesheet" href="style.css"> 
+<style>
+body {
+    background-color: #CC0303; 
+}
+
+</style>
+<link rel="stylesheet" href="style.css"> 
+</head>
+
+    <body>
+   
+            <!-- NAVBAR    -->
+                <nav class="Rectangle3 ; stiky-top">
+                    <a class="navbar-brand" href="#">
+                    <img src="TEL 1.png" width="150" height="80" class="d-inline-block align-top" alt="">
+                    </a>
+                    <button type="Buttonhome" class="btn btn-outline-danger" style="width: 90px; height: 30px; left: 795px; top: 32px; position: absolute; color: #CC0303; font-size: 25.42px; font-family: Inter; font-weight: 500; line-height: 25.42px; word-wrap: break-word"> Home </button>
+                    <a class="About" style="width: 84px; height: 25px; left: 961px; top: 32px; position: absolute; color: black; font-size: 25.42px; font-family: Inter; font-weight: 500; line-height: 25.42px; word-wrap: break-word">About</a>
+                    <a class="Contact" style="width: 106px; height: 25px; left: 1113px; top: 30px; position: absolute; color: black; font-size: 25.42px; font-family: Inter; font-weight: 500; line-height: 25.42px; word-wrap: break-word">Contact</a>
+                    <img src="profil.png" style="width: 60px; height: 50px; left: 1320px; top: 17px; position: absolute">
+                    </div>
+                </nav>
+
+          
+
+                    <div class="GreetingsTelkomAdmins">Selamat Datang, Telkom Admins! </div>
+                        <div class="YourJourneyAtTeluten">Perjalanan Anda di Teluten dimulai di sini. Telusuri manajemen tenant yang lancar – mulai dari kontrak hingga detail properti dengan mudah. Selamat bekerja!</div>
+                    </div>
+    
+                    <img src="orang2.png" style="width: 547px; height: 668px; left: 1000px; top: 232px; position: relative" src="https://via.placeholder.com/687x668"/>
+    
+                    <div class="WhatSOnYourAgenda">
+                    Telkom Admins Teluten
+                    </div>
+
+                <div class="kotakfitur"></div> 
+                    <div class="kotakfitur2"></div>
+                        <div class="kotakfitur3"></div>
+                            <div class="kotakfitur4"></div>
+
+                <div class="Location">Location & Facilities</div>
+                        <div class="TenantData">Tenant Data</div>
+                            <div class="Streamline">Sederhanakan manajemen tenant – tambahkan, pantau, dan hapus penyewa dengan mudah.</div>
+                                <div class="EnhanceTenant">Tingkatkan kepuasan penyewa dengan menawarkan wawasan tentang lokasi tenant dan fasilitas tenant.</div>
+                                        <div class="FinancialDashboard">Financial Dashboard</div>
+                                             <div class="TakeFinancial">Tampilkan dasbor finansial – perbarui pembayaran dan tunjukkan keuntungan tenant, semuanya dalam satu dasbor.
+<br/></div>
+                
+                              <div class="TenantContract">Tenant Contract</div>
+                            <div class="Effortlessly">Kelola kontrak tenant dengan mudah – verifikasi perpanjangan, penghentian, dan mulai perjanjian baru.</div>
+               
+                                <img src="Group 1.png" style="width: 76px; height: 84px; left: 541px; top: 1422px; position: absolute">
+                                <img src="tangan.png" style="width: 88px; height: 96px; left: 982px; top: 1420px; position: absolute">
+                                <img src="Location pin.png" style="width: 80px; height: 80px; left: 533px; top: 1838px; position: absolute">
+                                <img src="Payment.png" style="width: 88px; height: 82px; left: 989px; top: 1843px; position: absolute">
+</div>
+
+</body>
+
+
